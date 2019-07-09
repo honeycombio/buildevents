b16669497ae94ff651c0bf35abdc73347c7d6fc0
Chris Toshok <toshok@hound.sh>
2019-07-09 18:47:06 -0500

    oops, include cmd_step.go

--- cmd_step.go
+++ cmd_step.go
@@ -20,7 +20,7 @@ one of "install", "before_script", "script", and so on. In CircleCI, this
 most closely maps to a single job. It should be run at the end of the step.`,
 		Args:                  cobra.ExactArgs(4),
 		DisableFlagsInUseLine: true,
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			traceID := strings.TrimSpace(args[0])
 			stepID := strings.TrimSpace(args[1])
 			startTime := parseUnix(strings.TrimSpace(args[2]))
@@ -40,6 +40,8 @@ most closely maps to a single job. It should be run at the end of the step.`,
 				"duration_ms":     time.Since(startTime) / time.Millisecond,
 			})
 			ev.Timestamp = startTime
+
+			return nil
 		},
 	}
 	return stepCmd
