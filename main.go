978552e39d7cd4a9f2eaab3ddc00d87b5ede842a
Chris Gardner <chris.gardner@section8games.co.uk>
2020-06-21 17:34:24 +0100

    Add support for Azure Piplines and some initial env vars

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ const (
 	providerGitLab           = "GitLab-CI"
 	providerJenkinsX         = "Jenkins-X"
 	providerGoogleCloudBuild = "Google-Cloud-Build"
+	providerAzurePipelines   = "Azure-Piplines"
 )
 
 func main() {
