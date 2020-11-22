2e511746268139f40e1a3876ab550d91b090aeac
Marilyn Franklin <marilyn.j.franklin@gmail.com>
2020-11-21 20:03:07 -0600

    Add support for overriding default fields

--- cmd_cmd.go
+++ cmd_cmd.go
@@ -62,10 +62,6 @@ will be launched via "bash -c" using "exec".`,
 			err := runCommand(subcmd)
 			dur := time.Since(start)
 
-			// Annotate with arbitrary fields after the command runs
-			// this way we can consume a file if the command itself generated one
-			arbitraryFields(*filename, ev)
-
 			ev.Add(map[string]interface{}{
 				"trace.parent_id": stepID,
 				"trace.span_id":   fmt.Sprintf("%x", spanBytes),
@@ -75,6 +71,10 @@ will be launched via "bash -c" using "exec".`,
 				"cmd":             subcmd,
 			})
 			ev.Timestamp = start
+
+			// Annotate with arbitrary fields after the command runs
+			// this way we can consume a file if the command itself generated one
+			arbitraryFields(*filename, ev)
 
 			if err == nil {
 				ev.AddField("status", "success")
--- cmd_step.go
+++ cmd_step.go
@@ -30,7 +30,6 @@ most closely maps to a single job. It should be run at the end of the step.`,
 			defer ev.Send()
 
 			providerInfo(*ciProvider, ev)
-			arbitraryFields(*filename, ev)
 
 			ev.Add(map[string]interface{}{
 				"trace.parent_id": traceID,
@@ -40,6 +39,8 @@ most closely maps to a single job. It should be run at the end of the step.`,
 				"duration_ms":     time.Since(startTime) / time.Millisecond,
 			})
 			ev.Timestamp = startTime
+
+			arbitraryFields(*filename, ev)
 
 			return nil
 		},
