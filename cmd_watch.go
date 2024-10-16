ef81eda2022d27df774f57fd9473cf14dea942d0
Liz Fong-Jones <lizf@honeycomb.io>
2024-10-16 10:51:01 -0700

    fix(watch): not_running is the new queued (#221)
    ## Which problem is this PR solving?
    
    We are seeing premature buildevents terminations because we see all jobs
    succeeded or failed, and ignore the not_running.
    
    ## Short description of the changes
    
    Treat the new not_running status the same as "queued".
    
    ## How to verify
    
    
    https://app.circleci.com/pipelines/github/honeycombio/hound/80521/workflows/d0f8b476-2b41-4aeb-93b0-428c3c46ed9c/jobs/1341656?invite=true#step-102-5277_75
    jobs like this should no longer show "succeeded" when there are still
    jobs left "not_running"
    
    we should stop seeing async traces like
    https://ui-dogfood.honeycomb.io/prod/environments/prod/datasets/build-events/result/Hne5hzeBSEu/trace/bP2CUXc6L59?source=query&span=d0f8b476-2b41-4aeb-93b0-428c3c46ed9c
    
    ---------
    
    Co-authored-by: Jamie Danielson <jamiedanielson@honeycomb.io>

--- cmd_watch.go
+++ cmd_watch.go
@@ -251,6 +251,9 @@ func evalWorkflow(client *circleci.Client, wfID string, jobName string) (anyRunn
 			// it's not configured to run this build (because of a tag or something)
 			anyBlocked = true
 			continue
+		case "not_running":
+			// not_running is the same as queued
+			fallthrough
 		case "queued":
 			// queued means a job is due to start running soon, so we consider it running
 			// already.
